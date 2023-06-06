08973ba92c5da4ccf85e19ed3a6ed38a86197d32
Lander Nunez <landernunez@gmail.com>
2023-06-05 17:01:26 -0700

    manulist change

--- ghi/app/src/ManuForm.js
+++ ghi/app/src/ManuForm.js
@@ -1,55 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-class ManuForm extends React.Component {
-    constructor (props) {
-        super(props)
-        this.state = {
-            name: '',
-        }
-        this.handleNameChange = this.handleNameChange.bind(this)
-        this.handleSubmit = this.handleSubmit.bind(this)
+const ManuForm = () => {const [name, setName] = useState('')
+
+const handleSubmit = async (event) => {
+    event.preventDefault()
+    const data = {name}
+    const manuUrl = 'http://localhost:8100/api/manufacturers/';
+    const fetchConfig = {
+      method: 'post',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+    const response = await fetch(manuUrl, fetchConfig);
+    if (response.ok) {
+      const newManu = await response.json();
+      console.log(newManu);
+
+      setName('');
     }
-    
+  };
 
-    async handleSubmit(event) {
-        event.preventDefault();
-        const data = {...this.state};
-        console.log(data);
+  const handleNameChange = (event) => {
+    const value = event.target.value;
+    setName(value);
+  };
+  
 
-        const manuUrl = 'http://localhost:8100/api/manufacturers/';
-        const fetchConfig = {
-          method: "post",
-          body: JSON.stringify(data),
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        };
-        const response = await fetch(manuUrl, fetchConfig);
-        if (response.ok) {
-          const newManu = await response.json();
-          console.log(newManu);
 
-          const cleared = {
-            name: '',
-          };
-          this.setState(cleared);
-        }
-    }
-    
-    handleNameChange(event) {
-        const value = event.target.value;
-        this.setState({name: value})
-    }
-
-    render () {
         return (
             <div className="row">
                 <div className="offset-3 col-6">
                     <div className="shadow p-4 mt-4">
-                        <h1>Create a manufacturer</h1>
-                        <form onSubmit={this.handleSubmit} id="create-manu-form">
+                        <h1>Add a Manufacturer</h1>
+                        <form onSubmit={handleSubmit} id="create-manu-form">
                         <div className="form-floating mb-3">
-                            <input onChange={this.handleNameChange} value={this.state.name} placeholder="Name" required type="text" name="name" id="name" className="form-control"/>
+                            <input onChange={handleNameChange} value={name} placeholder="Name" required type="text" name="name" id="name" className="form-control"/>
                             <label htmlFor="name">Name</label>
                         </div>                        
                         <button className="btn btn-primary">Create</button>
@@ -59,6 +46,5 @@ class ManuForm extends React.Component {
             </div>
         )
     }
-}
 
 export default ManuForm;
